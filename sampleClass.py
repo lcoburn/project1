21fd1d4ec7969fd324eaf3bf21a5b9f33aca55bd
lcoburn <lukecoburn@gmail.com>
2021-03-06 18:30:57 +0100

    Initial commit

---
+++ sampleClass.py
@@ -0,0 +1,20 @@
+
+
+class Class1():
+    def __init__(self):
+        pass
+    def method1(self):
+        print('Frau Dr Heer ist die Beste!')
+
+    def method2(self):
+        print('Herr Dr Luke ist der Zweitbeste!')
+
+
+class Class2():
+    def __init__(self):
+        pass
+    def method3(self):
+        print('Frau Dr Heer ist nicht die Beste!')
+
+    def method4(self):
+        print('Herr Dr Luke ist nicht der Zweitbeste!')
